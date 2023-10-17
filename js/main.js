e79b5d4b37c15fb2477c7a59e5b9f8e0c4d0388c
Olesia_Is <isichenko.ok@gmail.com>
2023-10-17 16:27:03 +0300

    Refactoring is completed

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-//Получение псевдослучайного числа из заданного интервала (min >=0), включая границы
-const getRandomInt = function (min = 0, max = 1) {
+const PHOTOS_QUANTITY = 25;
 
-  if (min < 0 || max < 0) {
-    return NaN;
-  }
-
-  if (min >= max) {
-    let swip = min;
-    min = max;
-    max = swip;
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const Likes = {
+  min: 15,
+  max: 200,
 };
 
-//Сравнение длины строки с максимальной длиной строки
-const checkStringLimit = (text, maxLenght) => {
-  return text.length <= maxLenght ? true : false;
-}
+const CommentsIdRange = {
+  min: 1,
+  max: 1000,
+};
+
+const CommentsQuantity = {
+  min: 1,
+  max: 5,
+};
+
+const AvatarsIdRange = {
+  min: 1,
+  max: 6,
+};
+
+const photosInformation = [];
 
 const commentTexts = [
   'Всё отлично!',
@@ -54,25 +54,40 @@ const descriptions = [
   'Чёрно-белый кот лежит в кресле',
   'Закат на пляже с видом на древний город',
   'Рассвет в лесу после дождя',
-]
+];
 
-const photoPostQuantity = 25;
-const minLikes = 15;
-const maxLikes = 200;
-let commentIds = [];
+//Получение псевдослучайного числа из заданного интервала (min >=0), включая границы
+const getRandomInt = function (min = 0, max = 1) {
 
-// Получения случайного количества комментариев в диапазоне от 1 до 5
-const commentsQuantity = () => { return getRandomInt(1, 5)};
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
+
+  if (min >= max) {
+    let swip = min;
+    min = max;
+    max = swip;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+//Сравнение длины строки с максимальной длиной строки
+const checkStringLimit = (text, maxLenght) => {
+  return text.length <= maxLenght ? true : false;
+};
 
 // Получение случайного элемента массива
 const getRandomArrayElement = (array) => {
   return array[getRandomInt(0, array.length - 1)];
-}
-
+};
 
 // Проверка числа на уникальность и запись в массив
-const createCommentId = (array) => {
-  const createId = () => { return getRandomInt(1, 1000) };
+const getUniqueId = (array, from = 1, to = 100) => {
+  const createId = () => { return getRandomInt(from, to) };
   let id = createId();
 
   if (!array[0]) {
@@ -86,34 +101,36 @@ const createCommentId = (array) => {
 
   array[array.length] = id;
   return id;
-}
-
-
-// Создание комментария
-const createComment = () => {
-  return {
-    id: createCommentId(commentIds),
-    avatar: 'img/avatar-' + getRandomInt(1, 6) + '.svg',
-    message: getRandomArrayElement(commentTexts),
-    name: getRandomArrayElement(names),
-  };
-}
-
-// Создание массива со случайным количеством комментариев в диапазоне от 1 до 5
-const createSeveralComments = () => {
-  return new Array(commentsQuantity()).fill(null).map(() => createComment());
-}
-
-// Создание объекта с описанием фотографии
-const createPhotoInformation = (idNumber = 1) => {
-  return {
-    id: idNumber,
-    url: 'photos/' + idNumber + '.jpg',
-    description: getRandomArrayElement(descriptions),
-    likes: getRandomInt(minLikes, maxLikes),
-    comments: createSeveralComments(),
-  };
 };
 
-// Получение массива с объектами с опиманием фотографий
-const photos = new Array(photoPostQuantity).fill(null).map((value, index) => createPhotoInformation(index + 1));
+// Создание массива объектов комментариев
+const createComments = () => {
+  const comments = [];
+  const commentIds = [];
+
+  for (let i = 0; i < getRandomInt(CommentsQuantity.min, CommentsQuantity.max); i++) {
+    comments.push({
+      id: getUniqueId(commentIds, CommentsIdRange.min, CommentsIdRange.max),
+      avatar: 'img/avatar-' + getRandomInt(AvatarsIdRange.min, AvatarsIdRange.max) + '.svg',
+      message: getRandomArrayElement(commentTexts),
+      name: getRandomArrayElement(names),
+    });
+  }
+
+  return comments;
+};
+
+// Создание массива объектов с информацией о фотографиях
+const createPhotosInformation = () => {
+  for (let i = 0; i < PHOTOS_QUANTITY; i++) {
+    photosInformation.push({
+      id: (i + 1),
+      url: 'photos/' + (i + 1) + '.jpg',
+      description: getRandomArrayElement(descriptions),
+      likes: getRandomInt(Likes.min, Likes.max),
+      comments: createComments(),
+    });
+  }
+
+  return photosInformation;
+};
