3182a6a1c2debd8662b7e2079eba841d99c9b202
Olesia_Is <isichenko.ok@gmail.com>
2023-10-19 15:51:03 +0300

    Refactoring is completed. Modules were created based on previous main.js

---
+++ js/data.js
@@ -0,0 +1,99 @@
+import { getRandomInt, getNumbersArray, getRandomArrayElement, shuffleArray } from './util.js';
+
+const PHOTOS_QUANTITY = 25;
+
+const Likes = {
+  min: 15,
+  max: 200,
+};
+
+const CommentsIdRange = {
+  min: 1,
+  max: 1000,
+};
+
+const CommentsQuantity = {
+  min: 1,
+  max: 5,
+};
+
+const AvatarsIdRange = {
+  min: 1,
+  max: 6,
+};
+
+let currentCommentId = CommentsIdRange.min;
+const photosInformation = [];
+
+const commentTexts = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const names = [
+  'Валерия',
+  'Виктор',
+  'Мирослава',
+  'Андрей',
+  'Ева',
+  'Давид',
+  'Григорий',
+  'Родион',
+  'Василиса',
+  'Константин',
+  'Матвей',
+  'София',
+  'Екатерина',
+  'Полина',
+];
+
+const descriptions = [
+  'Старинные часы на стене здания',
+  'Мужчина серфит на волне',
+  'Чёрно-белый кот лежит в кресле',
+  'Закат на пляже с видом на древний город',
+  'Рассвет в лесу после дождя',
+];
+
+// Создание массива объектов комментариев
+const createComments = (array) => {
+  const comments = [];
+
+  for (let i = 0; i < getRandomInt(CommentsQuantity.min, CommentsQuantity.max); i++) {
+    comments.push({
+      id: array[currentCommentId],
+      avatar: 'img/avatar-' + getRandomInt(AvatarsIdRange.min, AvatarsIdRange.max) + '.svg',
+      message: getRandomArrayElement(commentTexts),
+      name: getRandomArrayElement(names),
+    });
+
+    currentCommentId++;
+  }
+
+  return comments;
+};
+
+// Создание массива объектов с информацией о фотографиях
+const createPhotosInformation = () => {
+  currentCommentId = CommentsIdRange.min;
+  const commentsIDs = getNumbersArray(CommentsIdRange.min, CommentsIdRange.max);
+  shuffleArray(commentsIDs);
+
+  for (let i = 0; i < PHOTOS_QUANTITY; i++) {
+    photosInformation.push({
+      id: (i + 1),
+      url: 'photos/' + (i + 1) + '.jpg',
+      description: getRandomArrayElement(descriptions),
+      likes: getRandomInt(Likes.min, Likes.max),
+      comments: createComments(commentsIDs),
+    });
+  }
+
+  return photosInformation;
+};
+
+export { createPhotosInformation };
---
+++ js/util.js
@@ -0,0 +1,49 @@
+//Получение псевдослучайного числа из заданного интервала (min >=0), включая границы
+const getRandomInt = function (min = 0, max = 1) {
+
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
+
+  if (min >= max) {
+    let swip = min;
+    min = max;
+    max = swip;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+//Сравнение длины строки с максимальной длиной строки
+const checkStringLimit = (text, maxLenght) => {
+  return text.length <= maxLenght ? true : false;
+};
+
+// Получение массива чисел от min до max
+const getNumbersArray = (min, max) => {
+  const numbersArray = [];
+
+  for (let i = 0; i <= (max - min); i++) {
+    numbersArray[i] = i + min;
+  }
+
+  return numbersArray;
+}
+
+// Получение случайного элемента массива
+const getRandomArrayElement = (array) => {
+  return array[getRandomInt(0, array.length - 1)];
+};
+
+// Перемешивание элементов массива в случайном порядке
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+export { getRandomInt,  checkStringLimit, getRandomArrayElement, shuffleArray, getNumbersArray };
